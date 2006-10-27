e27eec5d988c25a73136c2fe6657dd65089a32d4
Dan Kogai <dankogai+cpan@gmail.com>
2006-10-27 04:13:09 +0000

    Import of DANKOGAI/BSD-getloadavg-0.02 from CPAN.
    gitpan-cpan-distribution: BSD-getloadavg
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         DANKOGAI/BSD-getloadavg-0.02.tar.gz
    gitpan-cpan-author:       DANKOGAI
    gitpan-cpan-maturity:     released

--- lib/BSD/getloadavg.pm
+++ lib/BSD/getloadavg.pm
@@ -3,8 +3,8 @@ use 5.006002;
 use strict;
 use warnings;
 
-our $VERSION = sprintf "%d.%02d", q$Revision: 0.1 $ =~ /(\d+)/g;
-
+our $VERSION = sprintf "%d.%02d", q$Revision: 0.2 $ =~ /(\d+)/g;
+our $DEBUG   = 0;
 require Exporter;
 our @ISA         = qw(Exporter);
 our @EXPORT      = qw( getloadavg );
@@ -17,6 +17,12 @@ XSLoader::load('BSD::getloadavg', $VERSION);
 
 sub getloadavg(){ 
     my $retval = xs_getloadavg();
+    if ($DEBUG){
+	eval {
+	    require Devel::Peek;
+	    Devel::Peek::Dump($retval);
+	}
+    }
     wantarray ? @{ $retval } : $retval->[0];
 }
 
@@ -44,13 +50,68 @@ You can simply
   use BSD::getloadavg;
   my @loadavg = getloadavg();
 
+=head1 COMPATIBILITY
+
+Though named BSD::getloadavg, this module also works on Linux and
+other platforms that support getloadavg.  To find if your platform
+supports this module, Just
+
+  nm /usr/lib/libc.* | grep getloadavg
+
+=head1 BENCHMARK
+
+You can run benchmark with t/benchmark.pl.  Here is the result in my platforms.
+
+=over 2
+
+=item MacBook Pro 2GHz, Mac OS X 10.4.8
+
+  Benchmark: running XS, command for at least 3 CPU seconds...
+          XS:  3 wallclock secs ( 1.40 usr +  1.60 sys =  3.00 CPU) @ 69808.00/s (n=209424)
+     command: 16 wallclock secs ( 0.58 usr  2.77 sys +  2.30 cusr  9.68 csys = 15.33 CPU) @ 1060.60/s (n=3553)
+           Rate command      XS
+  command  1061/s      --    -98%
+  XS      69808/s   6482%      --
+
+=item Dual Xeon 2.8GHz, FreeBSD 6-Stable
+
+  Benchmark: running XS, command for at least 3 CPU seconds...
+          XS:  4 wallclock secs ( 2.49 usr +  0.58 sys =  3.07 CPU) @ 139022.98/s (n=426844)
+     command: 11 wallclock secs ( 0.19 usr  2.98 sys +  2.34 cusr  7.12 csys = 12.62 CPU) @ 919.07/s (n=2908)
+              Rate command      XS
+  command    919/s      --    -99%
+  XS      139023/s  15026%      --
+
+=back
+
 =head2 EXPORT
 
 getloadavg()
 
 =head1 SEE ALSO
 
-getloadavg(5), L<http://www.freebsd.org/cgi/man.cgi?query=getloadavg&sektion=3>
+=over 2
+
+=item getloadavg(5)
+
+Also available as
+L<http://www.freebsd.org/cgi/man.cgi?query=getloadavg&sektion=3>.
+Since getloadavg is in libc in most platform, Makefile.PL needed no
+tweaking.  If you find a platform that does not have getloadavg in
+libc, please report.
+
+=item L<Sys::CpuLoad>
+
+This module accesses load average via /proc or uptime command.
+Therefore the performance is analogous to the benchmark above.
+
+=item L<BSD::Sysctl>
+
+Seems like the most versatile module in this arena.  Unfortunately
+most-platform dependent also.  Works only on FreeBSD as of Version
+0.06.
+
+=back
 
 =head1 AUTHOR
 
@@ -63,6 +124,5 @@ Copyright (C) 2006 by Dan Kogai
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself, either Perl version 5.8.8 or,
 at your option, any later version of Perl 5 you may have available.
-
 
 =cut
--- t/BSD-getloadavg.t
+++ t/BSD-getloadavg.t
@@ -1,5 +1,5 @@
 #
-# $Id: BSD-getloadavg.t,v 0.1 2006/10/26 11:20:29 dankogai Exp dankogai $
+# $Id: BSD-getloadavg.t,v 0.2 2006/10/27 04:11:25 dankogai Exp dankogai $
 #
 use strict;
 use warnings;
@@ -8,8 +8,8 @@ use Test::More tests => 5;
 #use Test::More qw/no_plan/;
 BEGIN { use_ok('BSD::getloadavg') };
 my @loadavg =   getloadavg();
-is scalar @loadavg, 3;
-like $loadavg[0], qr/^\d+\.\d+$/;
-like $loadavg[1], qr/^\d+\.\d+$/;
-like $loadavg[2], qr/^\d+\.\d+$/;
+is scalar @loadavg, 3, join(", ", @loadavg);
+like $loadavg[0], qr/^\d+\.\d+$/, $loadavg[0];
+like $loadavg[1], qr/^\d+\.\d+$/, $loadavg[1];
+like $loadavg[2], qr/^\d+\.\d+$/, $loadavg[2];
 
